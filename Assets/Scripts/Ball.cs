4c5af07d96a154bd35d6b1f7dfd4a7240a88dac1
enzardglitch <enzardthepioneer@gmail.com>
2026-07-18 15:04:59 +0700

    colors!

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+enum BallColor
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Purple,
+    Blue,
+    Orange,
+}
+
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+
+    [SerializeField]
+    private BallColor Color;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
